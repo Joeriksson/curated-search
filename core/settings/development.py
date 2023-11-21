dab36a791d6e77edf3333b4330cbccf273ea36a6
J-O Eriksson <25037079+Joeriksson@users.noreply.github.com>
2023-11-21 11:35:41 +0100

    update dependencies and fix django debug toolbar

--- core/settings/development.py
+++ core/settings/development.py
@@ -12,12 +12,34 @@ MIDDLEWARE += [
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-DEBUG_TOOLBAR_CONFIG = {
-    'JQUERY_URL': '',
-}
+# DEBUG_TOOLBAR_CONFIG = {
+#     'JQUERY_URL': '',
+# }
 
 
 # Django-debug-toolbar
 import socket
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
-INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ['127.0.0.1']
+
+print(f'INTERNAL_IPS: {INTERNAL_IPS}')
+
+# INTERNAL_IPS = [
+#     "127.0.0.1",
+# ]
+
+import mimetypes
+mimetypes.add_type("application/javascript", ".js", True)
+
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+}
+
+# The following is necessary to get the debug toolbar to work with Docker in this project.
+# TBD: Figure out why this is necessary.
+def show_toolbar(request):
+    return True
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
+}
+
