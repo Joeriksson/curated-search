190e460e146aa569911ac87ee482190a4c9cd099
J-O Eriksson <25037079+Joeriksson@users.noreply.github.com>
2025-03-19 19:36:04 +0100

    Add search functionality with SearchResultsList view and improve code consistency

--- feed/views.py
+++ feed/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
-
-from django.views.generic import View
+from django.contrib.postgres.search import SearchVector
 from django.core.management import call_command
 from django.http import HttpResponse
+from django.views.generic import View, ListView
+
+from feed.models import Entry
 
 
 class UpdateFeedView(View):
-
     def get(self, *args, **kwargs):
-        call_command('update_feeds')
+        call_command("update_feeds")
 
-        return HttpResponse('OK')
+        return HttpResponse("OK")
 
 
 class UpdateContentView(View):
-
     def get(self, *args, **kwargs):
-        call_command('update_content')
+        call_command("update_content")
 
-        return HttpResponse('OK')
+        return HttpResponse("OK")
+
+
+class SearchResultsList(ListView):
+    model = Entry
+    context_object_name = "entries"
+    template_name = "feed/search.html"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        # return Quote.objects.filter(
+        #     quote__search=query
+        # )
+        return Entry.objects.annotate(
+            search=SearchVector("title", "summary", "content")
+        ).filter(search=query)
